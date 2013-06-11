9c7363a71e83c3e5a7f11b13e12c7e0e958e4f57
compwron <wordymusic@gmail.com>
2013-06-11 00:14:04 -0500

    dictionary from mac used to find all words with x letters that don't have letters that we don't have. next step is to make sure that letter count in word does not exceed available letter count

--- mixer.rb
+++ mixer.rb
@@ -1,3 +1,30 @@
 letters = ["T", "A", "I", "A", "I", "I", "C", "N", "E", "M", "D", "T"]
+letters_in_word = 8
 
 
+letters_not_accepted = ('A'..'Z').to_a
+letters.each {|letter|
+	letters_not_accepted.delete(letter)
+}
+
+# puts letters_not_accepted
+
+possibles = []
+
+File.open("words.txt") do |file|
+      file.each_line{ |word| 
+      	if(word.size == letters_in_word) then
+      		keep_word = true
+      		letters_not_accepted.each { |letter|
+      			if (word.include?(letter) || word.include?(letter.downcase)) then
+      				keep_word = false
+      				# puts "getting rid of: #{word}"
+      			end
+      		}
+      		if keep_word then possibles += [word] end
+      	end
+      }
+    end
+
+
+# possibles
